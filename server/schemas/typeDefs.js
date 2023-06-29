79e23a162a73a83fa79bde8df04ac178d5c63189
unknown <dudeomous@gmail.com>
2023-06-29 18:40:10 -0400

    added typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type Chore {
+    _id: ID
+    choreName: String!
+    time: Number!
+    score: Number!
+    completed: Boolean!
+}
+
+type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    score: Number!
+    chores: [Chore]
+}
+
+type Checkout {
+    session: ID
+  }
+
+type Auth {
+    token: ID
+    user: User
+  }
+
+  
+type Query {
+    chores: [Chore]
+    users: [User]
+    user(_id: ID): User
+    chore(_id: ID): Chore
+}
+
+
+
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    addChore(choreName: String!, time: String!, score: Number!): Chore
+    updateChore(_id: ID, score: Number!, time: Number!): Chore
+    login(email: String!, password: String!): Auth
+}
+`
+
+module.exports = typeDefs;
