86431273310e0eaeaadabc800aabc65c8b5cc8d3
Gevork Seropyan <seropyangevork@rocketmail.com>
2023-07-07 11:12:46 -0400

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 //   createHttpLink,
 // } from '@apollo/client';
 // import { setContext }from '@apollo/client/link/context';
+import Profile from './pages/Profile';
 import Login from './pages/Login';
 import Chorepage from './pages/Chorepage';
 import HomePage from './pages/HomePage';
 import Navbar from './components/Navbar';
+
+
 // const httpLink = createHttpLink({
 //   uri: '/graphql',
 // });
@@ -39,16 +42,17 @@ function App() {
   // <ApolloProvider client={client}>
   <>
     <Router>
-      <Navbar>
+      {/* <Navbar> */}
         <div className='container'>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/Login" element={<Login />} />
+            <Route path="/Profile" element={<Profile />}/>
           </Routes>
         </div>
-      </Navbar>
+      {/* </Navbar> */}
     </Router>
-    </>
+  </>
   // </ApolloProvider>
 );
 }
