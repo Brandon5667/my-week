5d1789771cb72ccb8639d8a332e8729295b8c858
Brandon Cullifer <brandon.cullifer@gmail.com>
2023-07-06 20:37:32 -0400

    added the survey model

---
+++ server/models/Survey.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const surveySchema = new Schema({
+    trash: {
+        type: Number,
+        required: true
+      },
+    
+      dishes: {
+        type: Number,
+        required: true
+      },
+
+      bathroom: {
+        type: Number,
+        required: true
+      },
+
+      walk: {
+        type: Number,
+        required: true
+      },
+
+      floor: {
+        type: Number,
+        required: true
+      }
+});
+
+const Survey = mongoose.model("Chore", surveySchema);
+
+module.exports = Survey;
