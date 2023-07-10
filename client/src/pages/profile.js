8d7d28145a8ba7e41f9d98da770ec19ed99e0ef5
Gevork Seropyan <seropyangevork@rocketmail.com>
2023-07-10 18:55:39 -0400

    "changes made in profile"

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import React, { useState, useEffect } from 'react';
-
+// import ChoreBlock from '../components/ChoreBlock';
+import {GET_ME} from '../utils/queries'
 
 const Profile = () =>{
+  const { loading, error, data } = useQuery(GET_ME);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
 
+  useEffect(() => {
+    if (data && data.me) {
+      setUsername(data.me.username);
+      setEmail(data.me.email);
+    }
+  }, [data]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
     return(
         <div>
             <h2>Profile</h2>
-            <p>Username:{}</p>
-            <p>Email:{}</p>
+            <p>Username:{username}</p>
+            <p>Email:{email}</p>
             <h3>Assigned Chores:</h3>
             <ul>
                 {chores.map((chore) => (
