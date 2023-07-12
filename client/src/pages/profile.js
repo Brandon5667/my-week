de21087b311146b45232bf48867e77a7de699429
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-07-12 18:10:02 -0400

    fix query for GET_ME

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import {GET_ME} from '../utils/queries'
-import {DELETE_USER} from '../utils/mutations'
-import { useQuery, useMutation } from '@apollo/client';
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import Auth from '../utils/auth';
+import React, { useState, useEffect } from "react";
+import { GET_ME } from "../utils/queries";
+import { DELETE_USER } from "../utils/mutations";
+import { useQuery, useMutation } from "@apollo/client";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import Auth from "../utils/auth";
 
-const Profile = () =>{
+const Profile = () => {
   const { loading, error, data } = useQuery(GET_ME);
   const [deleteAccount] = useMutation(DELETE_USER);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     if (data && data.me) {
@@ -28,14 +28,14 @@ const Profile = () =>{
   const handleDeleteAccount = async () => {
     try {
       await deleteAccount();
-      window.location.assign('/')
-      Auth.logout()
+      window.location.assign("/");
+      Auth.logout();
     } catch (error) {
       console.log("Error deleting account:", error.message);
     }
   };
-    return(
-      <Container className="d-flex align-items-center justify-content-center h-100 ">
+  return (
+    <Container className="d-flex align-items-center justify-content-center h-100 ">
       <Row className="d-flex justify-content-center">
         <Col xs={12} sm={12} md={12} lg={12}>
           <div className="profile-card">
@@ -48,14 +48,18 @@ const Profile = () =>{
               <label className="form-label">Email:</label>
               <p className="card-text">{email}</p>
             </div>
-            <Button variant="danger" className="profile-button" onClick={handleDeleteAccount}>
+            <Button
+              variant="danger"
+              className="profile-button"
+              onClick={handleDeleteAccount}
+            >
               Delete Account
             </Button>
           </div>
         </Col>
       </Row>
     </Container>
-    )
-}
+  );
+};
 
-export default Profile
+export default Profile;
