891b96704dc3838de0ff485491a5e6760210d97e
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-07-13 09:16:11 -0400

    add chore page to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -25,6 +25,9 @@ const Navbar = () => {
               <Link className="btn btn-lg btn-primary m-2" to="/profile">
                 View My Profile
               </Link>
+              <Link className="btn btn-lg btn-primary m-2" to="/chorepage">
+                Chore Page
+              </Link>
               <button className="btn btn-lg btn-light m-2" onClick={logout}>
                 Logout
               </button>
